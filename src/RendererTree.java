3b2fa25932eb38f80481ddeca0afce62230ff92c
eeguskiza <e.eguskiza@opendeusto.es>
2023-10-29 23:44:56 +0100

    commit

--- src/RendererTree.java
+++ src/RendererTree.java
@@ -4,11 +4,9 @@ import javax.swing.tree.DefaultTreeCellRenderer;
 import java.awt.*;
 import java.util.Map;
 
-
-
-
 public class RendererTree extends DefaultTreeCellRenderer {
     private Map<String, Integer> poblacionPorProvincia;
+    private JProgressBar progressBar = new JProgressBar();
 
     public RendererTree(Map<String, Integer> poblacionPorProvincia) {
         this.poblacionPorProvincia = poblacionPorProvincia;
@@ -16,16 +14,18 @@ public class RendererTree extends DefaultTreeCellRenderer {
 
     @Override
     public Component getTreeCellRendererComponent(JTree tree, Object value, boolean selected, boolean expanded, boolean leaf, int row, boolean hasFocus) {
-        Component c = super.getTreeCellRendererComponent(tree, value, selected, expanded, leaf, row, hasFocus);
-        if (c instanceof JLabel) {
-            JLabel label = (JLabel) c;
+        if (value instanceof DefaultMutableTreeNode) {
             DefaultMutableTreeNode node = (DefaultMutableTreeNode) value;
-            if (node.isLeaf()) {
-                String provincia = value.toString();
+            if (node.isLeaf()) { // Para provincias
+                String provincia = node.getUserObject().toString();
                 int poblacion = poblacionPorProvincia.getOrDefault(provincia, 0);
-                label.setText(provincia + " (" + poblacion + ")");
+                progressBar.setString(provincia + " (" + poblacion + ")");
+                progressBar.setStringPainted(true);
+                progressBar.setValue(poblacion); // Aquí puedes ajustar el valor según tu rango
+                progressBar.setMaximum(1000000); // Asumiendo que este es el máximo, ajusta según tus datos
+                return progressBar;
             }
         }
-        return c;
+        return super.getTreeCellRendererComponent(tree, value, selected, expanded, leaf, row, hasFocus);
     }
 }
